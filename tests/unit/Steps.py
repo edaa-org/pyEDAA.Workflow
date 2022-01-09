b463acf67058f7f59b4e6281f99f1e6ef05dcfd3
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2022-01-09 12:11:36 +0100

    Added unit test.

---
+++ tests/unit/Steps.py
@@ -0,0 +1,52 @@
+# ==================================================================================================================== #
+#              _____ ____    _        _  __        __         _     __ _                                               #
+#  _ __  _   _| ____|  _ \  / \      / \ \ \      / /__  _ __| | __/ _| | _____      __                                #
+# | '_ \| | | |  _| | | | |/ _ \    / _ \ \ \ /\ / / _ \| '__| |/ / |_| |/ _ \ \ /\ / /                                #
+# | |_) | |_| | |___| |_| / ___ \  / ___ \ \ V  V / (_) | |  |   <|  _| | (_) \ V  V /                                 #
+# | .__/ \__, |_____|____/_/   \_\/_/   \_(_)_/\_/ \___/|_|  |_|\_\_| |_|\___/ \_/\_/                                  #
+# |_|    |___/                                                                                                         #
+# ==================================================================================================================== #
+# Authors:                                                                                                             #
+#   Patrick Lehmann                                                                                                    #
+#                                                                                                                      #
+# License:                                                                                                             #
+# ==================================================================================================================== #
+# Copyright 2014-2021 Patrick Lehmann - Bötzingen, Germany                                                             #
+#                                                                                                                      #
+# Licensed under the Apache License, Version 2.0 (the "License");                                                      #
+# you may not use this file except in compliance with the License.                                                     #
+# You may obtain a copy of the License at                                                                              #
+#                                                                                                                      #
+#   http://www.apache.org/licenses/LICENSE-2.0                                                                         #
+#                                                                                                                      #
+# Unless required by applicable law or agreed to in writing, software                                                  #
+# distributed under the License is distributed on an "AS IS" BASIS,                                                    #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                                             #
+# See the License for the specific language governing permissions and                                                  #
+# limitations under the License.                                                                                       #
+#                                                                                                                      #
+# SPDX-License-Identifier: Apache-2.0                                                                                  #
+# ==================================================================================================================== #
+#
+"""Unt tests for ``Step`` classes."""
+from unittest import TestCase
+
+from pyEDAA.Workflow.Steps    import PrepareEnvironment, CreateLibrary, Analyze, Elaborate, Simulate, View
+from pyEDAA.Workflow.Workflow import Workflow, Host
+
+
+class Simulation(TestCase):
+	def test_Steps(self):
+		host = Host()
+		workflow = Workflow("test flow", host)
+
+		steps = [
+			PrepareEnvironment("prepare environment", host, workflow),
+			CreateLibrary("create library", host, workflow),
+			Analyze("analyze", host, workflow),
+			Elaborate("elaborate", host, workflow),
+			Simulate("simulate", host, workflow),
+			View("view", host, workflow)
+		]
+		workflow.AppendSteps(steps)
+		workflow.Run()
