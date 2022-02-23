11d184af88e0d98e67f491e7d34956c5ad20b9ba
Patrick Lehmann <Paebbels@gmail.com>
2022-02-23 23:58:12 +0100

    First breakthrough with GHDL MinGW32-mcode.

--- tests/integration/GHDL.py
+++ tests/integration/GHDL.py
@@ -12,7 +12,7 @@ from pyEDAA.ProjectModel import Project, FileSet, VHDLSourceFile, VHDLLibrary
 from pyEDAA.ToolSetup import Installations
 from pyEDAA.ToolSetup.DataModel import Installation, Vendor, Tool, ToolInstance
 from pyEDAA.Workflow import Workflow, ExchangeObject, Host
-from pyEDAA.Workflow.Steps import PrepareEnvironment, CreateLibrary as _CreateLibrary
+from pyEDAA.Workflow.Steps import PrepareEnvironment, CreateLibrary as _CreateLibrary, Elaborate as _Elaborate, Simulate as _Simulate
 
 
 class CreateAndAnalyzeLibrary(_CreateLibrary):
@@ -44,34 +44,97 @@ class CreateAndAnalyzeLibrary(_CreateLibrary):
 		analyzer[analyzer.FlagMultiByteComments] = True
 		analyzer[analyzer.FlagSynopsys] = True
 
-		analyzer[analyzer.OptionPath] = Path(".")
+		analyzer[analyzer.OptionPath] = []
 
 		print(f"  Analyzing VHDL files into VHDL library '{library.Name}'")
 		for file in library.Files:
-			analyzer[analyzer.OptionPath].Value = file.ResolvedPath
+			analyzer[analyzer.OptionPath].Value = (file.ResolvedPath,)
 
-			arguments = analyzer.ToArgumentList()
-
+#			arguments = analyzer.ToArgumentList()
 			print(f"    Analyzing VHDL file '{file}'")
-			print(f"      {arguments}")
+#			print(f"      {arguments}")
+			analyzer.StartProcess()
+			for line in analyzer.GetLineReader():
+				print(f"        {line}")
+#			print(f"Exit code: {analyzer._process.returncode}")
+
+
+class Elaborate(_Elaborate):
+	def _Run(self):
+		project: Project = self._input["Project"]
+		toplevelLibraryName: str = self._input["VHDLLibrary"]
+		toplevelEntity: str      = self._input["TopLevel"]
+
+		design = project.DefaultDesign
+		toplevelLibrary = design.VHDLLibraries[toplevelLibraryName]
+
+		self._ElaborateLibrary(toplevelLibrary, toplevelEntity)
+
+	def _ElaborateLibrary(self, library: VHDLLibrary, toplevelName: str) -> None:
+		hdlSimulator: HDLSimulator = self._input["HDLSimulator"]
+		elaborator: GHDL = cast(GHDL, hdlSimulator.GetEloborator())
+		elaborator[elaborator.FlagVHDlStandard] = VHDLVersion.VHDL2008
+		elaborator[elaborator.FlagLibrary] = library.Name
+		elaborator[elaborator.FlagRelaxed] = True
+		elaborator[elaborator.FlagExplicit] = True
+		elaborator[elaborator.FlagSynopsys] = True
+		elaborator[elaborator.OptionTopLevel] = self._input["TopLevel"]
+
+#		arguments = elaborator.ToArgumentList()
+		print(f"  Elaborating VHDL toplevel '{library.Name}.{toplevelName}'")
+#		print(f"    {arguments}")
+
+		elaborator.StartProcess()
+		for line in elaborator.GetLineReader():
+			print(f"        {line}")
+
+
+class Simulate(_Simulate):
+	def _Run(self):
+		project: Project = self._input["Project"]
+		toplevelLibraryName: str = self._input["VHDLLibrary"]
+		toplevelEntity: str      = self._input["TopLevel"]
+
+		design = project.DefaultDesign
+		toplevelLibrary = design.VHDLLibraries[toplevelLibraryName]
+
+		self._SimulateLibrary(toplevelLibrary, toplevelEntity)
+
+	def _SimulateLibrary(self, library: VHDLLibrary, toplevelName: str) -> None:
+		hdlSimulator: HDLSimulator = self._input["HDLSimulator"]
+		simulator: GHDL = cast(GHDL, hdlSimulator.GetSimulator())
+		simulator[simulator.FlagVHDlStandard] = VHDLVersion.VHDL2008
+		simulator[simulator.FlagLibrary] = library.Name
+		simulator[simulator.FlagRelaxed] = True
+		simulator[simulator.FlagExplicit] = True
+		simulator[simulator.FlagSynopsys] = True
+		simulator[simulator.OptionTopLevel] = self._input["TopLevel"]
+
+#		arguments = simulator.ToArgumentList()
+		print(f"  Simulating VHDL toplevel '{library.Name}.{toplevelName}'")
+#		print(f"    {arguments}")
+
+		simulator.StartProcess()
+		for line in simulator.GetLineReader():
+			print(f"    {line}")
 
 
 class Analyze(TestCase):
 	def _CreateProject(self) -> Project:
 		# Create a Project by code
-		project = Project("ghdl", rootDirectory=Path("src"), vhdlVersion=VHDLVersion.VHDL2008)
+		project = Project("ghdl", rootDirectory=Path("."), vhdlVersion=VHDLVersion.VHDL2008)
 		dutLibrary = VHDLLibrary("design", project=project)
-		dutFileset = FileSet("design", project=project)
+		dutFileset = FileSet("design", directory=Path("src"), project=project)
 		dutFileset.AddFiles([
-			VHDLSourceFile(Path("counter.vhdl"), vhdlLibrary=dutLibrary),
-			VHDLSourceFile(Path("toplevel.vhdl"), vhdlLibrary=dutLibrary),
+			VHDLSourceFile(Path("Counter.vhdl"), vhdlLibrary=dutLibrary),
+			VHDLSourceFile(Path("TopLevel.vhdl"), vhdlLibrary=dutLibrary),
 		])
 
 		testLibrary = VHDLLibrary("test", project=project)
 		testLibrary.AddDependency(dutLibrary)
-		testFileset = FileSet("testbench", project=project)
+		testFileset = FileSet("testbench", directory=Path("tb"), project=project)
 		testFileset.AddFiles([
-			VHDLSourceFile(Path("testbench.vhdl"), vhdlLibrary=testLibrary),
+			VHDLSourceFile(Path("Testbench.vhdl"), vhdlLibrary=testLibrary),
 		])
 
 		return project
@@ -83,8 +146,8 @@ class Analyze(TestCase):
 			#		CreateProject("GenerateProject", "generate project", host, workflow),
 			PrepareEnvironment("PrepareEnvironment", "prepare environment", host, workflow),
 			CreateAndAnalyzeLibrary("AnalyzeLibrary", "create and analyze library", host, workflow),
-			# Elaborate("Elaborate", "elaborate", host, workflow),
-			# Simulate("Simulate", "simulate", host, workflow),
+			Elaborate("Elaborate", "elaborate", host, workflow),
+			Simulate("Simulate", "simulate", host, workflow),
 			#	View("View", "view", host, workflow)
 		]
 		workflow.AppendSteps(steps)
@@ -111,6 +174,7 @@ class Analyze(TestCase):
 		input["WorkingDirectory"] = Path("temp").resolve()
 		input["Project"] = project
 		input["VHDLLibrary"] = "test"
+		input["TopLevel"] = "testbench"
 		input["HDLSimulator"] = ghdlInstallationInstance
 
 		workflow.Input = input
@@ -130,6 +194,7 @@ class Analyze(TestCase):
 		input["WorkingDirectory"] = Path("temp").resolve()
 		input["Project"] = project
 		input["VHDLLibrary"] = "test"
+		input["TopLevel"] = "testbench"
 		input["HDLSimulator"] = ghdlInstallationInstance
 		workflow.Input = input
 		workflow.Run()
